131f393648222d628c4424ce9e3e8b5a0a89ecf4
wangxcSean <24490615+blackwilliam@users.noreply.github.com>
2022-05-05 16:06:27 +0800

    contract branch

---
+++ src/lib.rs
@@ -0,0 +1,11 @@
+use near_sdk::borsh::{self, BorshDeserialize, BorshSerialize};
+use near_sdk::{env, near_bindgen};
+
+near_sdk::setup_alloc!();
+
+
+#[near_bindgen]
+#[derive(Default, BorshDeserialize, BorshSerialize)]
+pub struct Counter {
+    val: i8,
+}
